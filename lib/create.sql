e9be0cce890561c371498647a4f8322243d389d4
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-14 02:09:15 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE projects
+(id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date DATE, end_date DATE);
+
+CREATE TABLE users
+(id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE pledges
+(id INTEGER PRIMARY KEY, amount INTEGER, project_id INTEGER, user_id INTEGER);
